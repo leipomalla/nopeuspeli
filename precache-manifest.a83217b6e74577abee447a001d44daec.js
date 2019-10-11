9e58b90308aa96b7381dbf48a563632496138b09
leipomalla <50195846+leipomalla@users.noreply.github.com>
2019-10-11 10:59:55 +0300

    Updates

---
+++ precache-manifest.a83217b6e74577abee447a001d44daec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f3f714994b1801b0d1a19aed57c88d",
+    "url": "/nopeuspeli/index.html"
+  },
+  {
+    "revision": "1aa00809a78a150ea775",
+    "url": "/nopeuspeli/static/css/main.360d1f10.chunk.css"
+  },
+  {
+    "revision": "ed56c57ccc7d199084d6",
+    "url": "/nopeuspeli/static/js/2.81aea8aa.chunk.js"
+  },
+  {
+    "revision": "1aa00809a78a150ea775",
+    "url": "/nopeuspeli/static/js/main.504336d3.chunk.js"
+  },
+  {
+    "revision": "733cbaef9dc02da0aab5",
+    "url": "/nopeuspeli/static/js/runtime-main.8d3a3529.js"
+  }
+]);
